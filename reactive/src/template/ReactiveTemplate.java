a7fb9c36e98d0bfea03eedc22b992c67472bf1a5
Marco Antognini <antognini.marco@gmail.com>
2015-10-03 19:02:17 +0200

    Add draft for offline setup

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -28,6 +28,50 @@ public class ReactiveTemplate implements ReactiveBehavior {
 
 		this.random = new Random();
 		this.pPickup = discount;
+
+		// Number of cities & moves:
+		Integer N = topology.size();
+		// Number of task's state: N+1 (when task == N it means no task is available)
+
+		// Potential V for each state s = (city, task)
+		Double potential[][] = new Double[N][N + 1];
+
+		Boolean progress = true;
+		do {
+			// Progress is made if `potential` is updated
+			progress = false;
+
+			// For all states (city x task)
+			for (Integer city = 0; city < N; ++city) {
+				for (Integer task = 0; task <= N; ++task) {
+					Double bestQForAction = -1.0;
+
+					// For all actions (move): compute best Q
+					for (Integer move = 0; move < N; ++move) {
+						Double q = reward(city, task, move);
+
+						// for all s'∈ S: γ * T(s,a,s') * V(s')
+						Integer cityP = move; // only one possibility for the next state's city
+						for (Integer taskP = 0; taskP <= N; ++taskP) {
+							q += gamma * probabilityTransition(city, task, move, cityP, taskP)
+									* potential[cityP][taskP];
+						}
+
+						// NOTE: probabilityTransition will probably depends only on the last two parameters
+
+						bestQForAction = Math.max(bestQForAction, q);
+					}
+
+					// Update potential
+					if (potential[city][task] != bestQForAction) {
+						progress = true;
+						potential[city][task] = bestQForAction;
+					}
+				}
+			}
+		} while (progress);
+
+		// TODO same `potential` as a field or something
 	}
 
 	@Override
