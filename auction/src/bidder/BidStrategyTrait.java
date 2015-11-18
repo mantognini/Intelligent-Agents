24762952b787fca25ea5772ba1600e63d2634899
Aerosh <christopher.salotti@epfl.ch>
2015-11-18 22:51:54 +0100

    Add bifHistory and possibility to add it

--- auction/src/bidder/BidStrategyTrait.java
+++ auction/src/bidder/BidStrategyTrait.java
@@ -1,9 +1,18 @@
 package bidder;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  * Base class for different bid strategies
  */
 public abstract class BidStrategyTrait {
+
+	/**
+	 * Store the bid history for all the current agents. The agent are identify by their index on bidList
+	 */
+	public HashMap<Integer, List<Long>> bidHistory = new HashMap<>();
+
 	/**
 	 * Given the number of tasks already owned with the corresponding current cost, and also the estimated cost if we
 	 * win the currently auctioned task, compute the desired bid value.
@@ -11,4 +20,13 @@ public abstract class BidStrategyTrait {
 	 * NB: the marginal cost is estimatedCost - currentCost.
 	 */
 	public abstract long bid(double marginalCost);
+
+	public void addBids(Long[] bids) {
+		for (int i = 0; i < bids.length; ++i) {
+			List<Long> currenbids = bidHistory.get(i);
+			currenbids.add(bids[i]);
+			bidHistory.put(i, currenbids);
+		}
+	}
+
 }
