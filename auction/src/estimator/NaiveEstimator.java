3ce53594e3e8283afc83d55fd5ca2f922a4cf7d3
Marco Antognini <antognini.marco@gmail.com>
2015-11-25 17:01:43 +0100

    Fix NaiveEstimator

--- auction/src/estimator/NaiveEstimator.java
+++ auction/src/estimator/NaiveEstimator.java
@@ -2,35 +2,22 @@ package estimator;
 
 import java.util.Random;
 
-import logist.Measures;
-import logist.agent.Agent;
-import logist.simulation.Vehicle;
 import logist.task.Task;
-import logist.topology.Topology.City;
 import planner.PlannerTrait;
-import utils.Utils;
 
-public class NaiveEstimator extends CostEstimatorTrait {
+public class NaiveEstimator extends NoFuture {
 
-	private Vehicle vehicle;
 	private Random random;
-	private City currentCity;
 
-	public NaiveEstimator(Agent agent) {
+	public NaiveEstimator() {
 		random = new Random(123458670);
-		vehicle = agent.vehicles().get(0);
-		currentCity = vehicle.getCurrentCity();
 	}
 
 	@Override
 	public double computeMC(PlannerTrait planner, Task task) {
-		// FIXME this doesn't take into account the task in the planner!!
-		Utils.ensure(vehicle.capacity() >= task.weight, "Task too heavy for the current vehicle");
-		long distanceTask = task.pickupCity.distanceUnitsTo(task.deliveryCity);
-		long distanceSum = distanceTask + currentCity.distanceUnitsTo(task.pickupCity);
-		double marginalCost = Measures.unitsToKM(distanceSum * vehicle.costPerKm());
-
+		double marginalCost = super.computeMC(planner, task);
 		double ratio = 1.0 + (random.nextDouble() * 0.05 * task.id);
+
 		return ratio * marginalCost;
 	}
 }
